263d1f01d712802e4e8925f3c500acabd834d4b3
ADFAST LTDA <143732424+adfastltda@users.noreply.github.com>
2024-11-10 14:58:39 -0300

    up

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!bin/bash/sh
+pkg update 
+pkg upgrade -y
+pkg install rust git
+rm -rf ~/RustyScanner
+cd ~
+git clone https://github.com/adfastltda/RustyScanner.git
+cd ~/RustyScanner/Scan/
+cargo build --release
+cp ~/RustyScanner/Scan/target/release/scanner $PREFIX/bin/scan
+rm -rf ~/RustyScanner
+scan -h
