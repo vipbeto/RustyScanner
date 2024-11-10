e8f648d5aa236639a9f3dc99832f08fbbd6bbddf
ADFAST LTDA <143732424+adfastltda@users.noreply.github.com>
2024-11-10 16:49:30 -0300

    up

--- install.sh
+++ install.sh
@@ -1,12 +1,38 @@
 #!/bin/sh
-pkg install rust -y
-pkg install git -y
-pkg install pkg-config -y
-rm -rf ~/RustyScanner
-cd ~
-git clone https://github.com/adfastltda/RustyScanner.git
+
+# Atualizando pacotes
+echo "Instalando pacotes necessários..."
+pkg install rust git pkg-config -y
+
+# Verificando se o diretório já existe e removendo se necessário
+if [ -d "$HOME/RustyScanner" ]; then
+  echo "Removendo diretório existente do RustyScanner..."
+  rm -rf ~/RustyScanner
+fi
+
+# Clonando o repositório
+echo "Clonando o repositório do RustyScanner..."
+git clone https://github.com/adfastltda/RustyScanner.git ~/RustyScanner
+
+# Verificando se o clone foi bem-sucedido
+if [ ! -d "$HOME/RustyScanner" ]; then
+  echo "Falha ao clonar o repositório!"
+  exit 1
+fi
+
+# Construindo o projeto
 cd ~/RustyScanner/Scan/
+echo "Compilando o RustyScanner..."
 cargo build --release
-cp ~/RustyScanner/Scan/target/release/scanner $PREFIX/bin/scan
+
+# Movendo o binário para o diretório $PREFIX/bin
+echo "Movendo o binário para $PREFIX/bin/scan..."
+cp target/release/scanner $PREFIX/bin/scan
+
+# Limpando o diretório do repositório clonado
+echo "Removendo diretório do repositório clonado..."
 rm -rf ~/RustyScanner
+
+# Testando a instalação
+echo "Instalação concluída! Testando o comando 'scan'..."
 scan -h
